65ccd6650623c78bc4181bb54047e71f1c10e820
mayyaf9-stack <mayyaf9@gmail.com>
2026-03-27 18:40:20 +0300

    Implement crime type mapper in mapper_task2.py
    Add a mapper to process crime type data from stdin.

--- milestone1/task2_crime_type/mapper_task2.py
+++ milestone1/task2_crime_type/mapper_task2.py
@@ -1,1 +1,8 @@
-
+import sys
+for line in sys.stdin:
+  columns = line.strip().split(",")
+  if columns[0] == "ID":
+    continue
+  if len(columns) < 6:
+    continue
+  print(f"{columns[5].strip()}\t1")
