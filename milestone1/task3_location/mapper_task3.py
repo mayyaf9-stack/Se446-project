27da4a3fc92659f78bc23f2f87bab379dcf34f99
nas65874-inas <nas65874@gmail.com>
2026-03-27 23:44:56 +0300

    Update mapper_task3.py

--- milestone1/task3_location/mapper_task3.py
+++ milestone1/task3_location/mapper_task3.py
@@ -1,1 +1,9 @@
+import sys
 
+for line in sys.stdin:
+    columns = line.strip().split(",")
+    if columns[0] == "ID":
+        continue
+    if len(columns) < 8:
+        continue
+    print(f"{columns[7].strip()}\t1")
